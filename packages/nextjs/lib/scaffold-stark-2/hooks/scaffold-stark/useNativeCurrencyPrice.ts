6c4679842b9d9d7d062c2c9e434a1aa90b340b21
iossocket <avx302@gmail.com>
2024-11-17 23:10:29 +0800

    refactoring price cache logic

--- packages/nextjs/lib/scaffold-stark-2/hooks/scaffold-stark/useNativeCurrencyPrice.ts
+++ packages/nextjs/lib/scaffold-stark-2/hooks/scaffold-stark/useNativeCurrencyPrice.ts
@@ -1,40 +1,21 @@
-import { useEffect } from "react";
-import { useTargetNetwork } from "./useTargetNetwork";
-import scaffoldConfig from "@scaffold-stark-2/scaffold.config";
 import { useGlobalState } from "@scaffold-stark-2/services/store/store";
-import { fetchPriceFromCoingecko } from "@scaffold-stark-2/utils/scaffold-stark";
-import { useInterval } from "usehooks-ts";
+import { priceService } from "@scaffold-stark-2/services/web3/PriceService";
+import { useEffect, useRef } from "react";
 
-/**
- * Get the price of Native Currency based on Native Token/DAI trading pair from Uniswap SDK
- */
 export const useNativeCurrencyPrice = () => {
-  const { targetNetwork } = useTargetNetwork();
-  const nativeCurrencyPrice = useGlobalState(state => state.nativeCurrencyPrice);
-  const strkCurrencyPrice = useGlobalState(state => state.strkCurrencyPrice);
-  const setNativeCurrencyPrice = useGlobalState(state => state.setNativeCurrencyPrice);
-  const setStrkCurrencyPrice = useGlobalState(state => state.setStrkCurrencyPrice);
-  // Get the price of ETH & STRK from Coingecko on mount
+  const setNativeCurrencyPrice = useGlobalState(
+    (state) => state.setNativeCurrencyPrice,
+  );
+  const setStrkCurrencyPrice = useGlobalState(
+    (state) => state.setStrkCurrencyPrice,
+  );
+  const ref = useRef<string>(priceService.getNextId().toString());
   useEffect(() => {
-    (async () => {
-      if (nativeCurrencyPrice == 0) {
-        const price = await fetchPriceFromCoingecko("ETH");
-        setNativeCurrencyPrice(price);
-      }
-      if (strkCurrencyPrice == 0) {
-        const strkPrice = await fetchPriceFromCoingecko("STRK");
-        setStrkCurrencyPrice(strkPrice);
-      }
-    })();
-  }, [targetNetwork]);
-
-  // Get the price of ETH & STRK from Coingecko at a given interval
-  useInterval(async () => {
-    const price = await fetchPriceFromCoingecko("ETH");
-    setNativeCurrencyPrice(price);
-    const strkPrice = await fetchPriceFromCoingecko("STRK");
-    setStrkCurrencyPrice(strkPrice);
-  }, scaffoldConfig.pollingInterval);
-
-  //return nativeCurrencyPrice;
+    const id = ref.current;
+    priceService.startPolling(id, setNativeCurrencyPrice, setStrkCurrencyPrice);
+    return () => {
+      priceService.stopPolling(id);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 };
